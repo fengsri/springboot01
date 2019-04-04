072a91ea547899d9846255b3313584147820289b
fengwen <1477452395@qq.com>
2019-04-04 11:04:47 +0800

    usercontroller has fix

--- src/main/java/com/gyf/web/controller/UserController.java
+++ src/main/java/com/gyf/web/controller/UserController.java
@@ -1,16 +1,12 @@
 package com.gyf.web.controller;
 
-import com.gyf.model.User;
 import com.gyf.service.UserServiceImpl;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller //声明Rest风格的控制器
 @RequestMapping("user")
@@ -19,9 +15,9 @@ public class UserController {
     private UserServiceImpl userService;
 
 
-    @RequestMapping("register")
+    @GetMapping("register")
     @ResponseBody
-    public String register(String username,String password){
+    public String register(@RequestParam("username") String username,@RequestParam("password") String password){
         //把数据保存到test1数据库
         userService.register(username,password);
         return "success";
